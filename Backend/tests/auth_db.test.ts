7515ee0bed11a36b3d37d09fad9afdc95388f738
jizuuu <giovisoria3@gmail.com>
2025-01-25 23:21:42 +0800

    add crud test.

--- Backend/tests/auth_db.test.ts
+++ Backend/tests/auth_db.test.ts
@@ -1,15 +1,14 @@
 import {describe, beforeAll, afterAll, beforeEach, afterEach, it, expect} from 'bun:test'
 import {PostgresJSDialect} from 'kysely-postgres-js'
 import postgres from 'postgres'
-import {Kysely, Migrator, FileMigrationProvider, NO_MIGRATIONS, PostgresDialect} from 'kysely'
+import {Kysely, Migrator, FileMigrationProvider, NO_MIGRATIONS, sql} from 'kysely'
 import {type DB} from 'kysely-codegen'
 import * as path from 'node:path'
 import { promises as fs } from 'node:fs'
-import { Pool } from 'pg'
 
 describe("database authentication", () => {
   let db:Kysely<DB>| undefined = undefined;
-  const postgresDB = new Pool({
+  const postgresDB = postgres({
     database: 'postgres',
     host: 'localhost',
     max: 10,
@@ -20,8 +19,8 @@ describe("database authentication", () => {
   let migrator:Migrator | undefined = undefined;
   beforeAll( async () => {
     db = new Kysely({
-      dialect: new PostgresDialect({
-        pool:postgresDB,
+      dialect: new PostgresJSDialect({
+        postgres:postgresDB,
     }), })
     migrator = new Migrator({
       db,
@@ -32,20 +31,42 @@ describe("database authentication", () => {
         migrationFolder: path.join(__dirname, '../migrations'),
       }),
     })
-    const migrationRes = await migrator.migrateToLatest()
-    console.log(migrationRes);
+    await migrator.migrateToLatest()
     afterAll( async () => {
-      const migrationResult = await migrator?.migrateDown()
-      console.log(migrationResult);
-      await postgresDB.end()
+      await migrator?.migrateTo(NO_MIGRATIONS)
+      await db?.destroy();
     })
   })
   
   it("it should create a user", async () => {
     const result = await db?.insertInto("auth.basic_user").values({
       email: "sample@email.com",
-      password: "sample123"
+      password: await Bun.password.hash("sample123")
     }).returningAll().executeTakeFirst()
-    console.log(result);
+    expect(result?.password).not.toBe("sample123")
+  })
+  it("it should find a user by email and password", async () => {
+    const result = await db?.selectFrom("auth.basic_user")
+      .select(["auth.basic_user.email", "auth.basic_user.id", "auth.basic_user.password"])
+      .where("auth.basic_user.email", "=", "sample@email.com")
+      .executeTakeFirst()
+    expect(await Bun.password.verify("sample123", result?.password || "")).toBeTrue()
+    if(result){
+      result.password = undefined as any;
+    }
+    expect(result?.email).toBe("sample@email.com")
+    expect(result?.password).toBeUndefined()
+  })
+  it("it should update the user password and updated_at", async () => {
+    const result = await db?.updateTable("auth.basic_user").set({
+      password: await Bun.password.hash("sample55"),
+      updated_at: sql`current_timestamp`
+    }).returningAll().executeTakeFirst()
+    expect(result?.password).not.toBe("sample55")
+    expect(result?.updated_at).not.toEqual(result?.created_at)
+  })
+  it("it should delete a user by email", async () => {
+    const result = await db?.deleteFrom("auth.basic_user").where("auth.basic_user.email", "=", "sample@email.com").executeTakeFirst();
+    expect(result?.numDeletedRows).toBe(1n);
   })
 })
